888f5fb26e09e061aa7ec1680660dd5b5574b541
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-01-02 13:05:21 -0300

    fix: Added assumeyes option in some DNF command lines

--- fedora.sh
+++ fedora.sh
@@ -15,10 +15,10 @@
 sudo sed -i '$ a \\n#- Added from fedora.sh install script -#\ndefaultyes=True\ndeltarpm=True\nfastestmirror=True\nkeepcache=True\nmax_parallel_downloads=10' /etc/dnf/dnf.conf
 
 # ENABLE RPM FUSION REPOSITORIES #
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # ENABLE FEDORA'S THIRD PARTY REPOSITORIES #
-sudo dnf install fedora-workstation-repositories
+sudo dnf install -y fedora-workstation-repositories
 
 
 #====================================
