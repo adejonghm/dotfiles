b77121aecdc68a65f3b4303007b23b0fdce8d170
Alejandro de Jongh <dejongh.morell@gmail.com>
2023-05-19 00:31:05 -0300

    feat: Added new app in the script installation

--- fedora.sh
+++ fedora.sh
@@ -67,7 +67,7 @@ gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg' > /etc/yum
 sudo dnf upgrade -y --refresh
 
 sudo dnf install -y bat dnf-plugins-core exa fd-find fzf gcc-c++ git mc neofetch npm patchutils pdfgrep python3-pip python3-tkinter ripgrep sqlite xkill zsh \
-cheese discord evince gedit gedit-plugin-textsize htop megasync mpv nomacs neovim pinta plank qbittorrent rpi-imager redshift redshift-gtk terminator sushi ulauncher vlc xfce4-notes-plugin xournalpp zeal \
+cheese discord evince gedit gedit-plugin-textsize htop megasync mpv nomacs neovim pinta plank qbittorrent rpi-imager redshift redshift-gtk simplescreenrecorder sushi terminator ulauncher vlc xfce4-notes-plugin xournalpp zeal \
 latexmk texlive texlive-{babel-english\*,babel-portuges\*,babel-spanish\*,base,bibtex,hyphenat,hyphenat-doc,picture} \
 brave-browser code containerd.io docker-ce docker-ce-cli docker-compose-plugin google-chrome-stable kubectl terraform \
 gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
